338059453c228c296439dfbda341cae63e38e985
Thanh Sang <anhsang31797@example.com>
2024-05-22 10:10:47 +0700

    generate code

--- form-signals/src/main.ts
+++ form-signals/src/main.ts
@@ -1,6 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+if (environment.prod) {
+  enableProdMode();
+}
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers:[importProvidersFrom(BrowserModule)]
+})
   .catch((err) => console.error(err));
